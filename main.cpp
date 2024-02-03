f80783634c4358fa47f0a645edffcab101dea7c1
AuroraRealm <3398817447@qq.com>
2024-02-03 18:11:37 +0800

    添加了基础功能

--- main.cpp
+++ main.cpp
@@ -1,13 +1,23 @@
-#include <time.h>
-#include <windows.h>
-#include <cstdio>
+/*
+ * @Author: AuroraRealm 3398817447@qq.com
+ * @Date: 2024-02-03 17:42:31
+ * @LastEditors: AuroraRealm 3398817447@qq.com
+ * @LastEditTime: 2024-02-03 18:04:01
+ * @FilePath: \A-small-program\main.cpp
+ */
+#include <stdio.h>
 #include <cstring>
+#include <ctime>
+#include <stdlib.h>
+#include <iostream>
+
+void sleep(int time) {clock_t head = clock();while (clock() - head <= time) { }}
 
 int main()
 {
   srand((unsigned)time(NULL));
-  printf("欢迎使用用于视窗系统的Pro 版本号:0.0.0.1 Alpha\n");
-  Sleep(1000);
+  printf("欢迎使用Program 版本号:0.0.0.1 Alpha\n");
+  sleep(1000);
   const char* str = "|/-\\";
   char bar[102];
   memset(bar, '\0', sizeof(bar));
@@ -17,13 +27,15 @@ int main()
     printf("[%-100s][%d%%][%c]\r", bar, i, str[i % 4]); // 格式控制
     fflush(stdout); // 刷新
     bar[i++] = '#'; // 填充数据
-    Sleep(rand() % 50); // 休眠
+    sleep(rand() % 50); // 休眠
   }
-  Sleep(1000);
-  system("clear");
+  sleep(1000);
   time_t ntime = time(0);
+  printf("\33c");
   printf("现在的时间为 %s\n", ctime(&ntime));
-  Sleep(1500);
-  system("clear");
+  sleep(1500);
+  printf("\33c");
+  printf("请输入您想要使用的功能\n\n");
+  printf("-> 1. 文本编辑器 <-");
   while (1){}
 }
